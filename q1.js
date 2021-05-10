5c385ee373b395013698df9ede1b66b8a45d72fc
adityabhosle-1 <68443991+adityabhosle-1@users.noreply.github.com>
2021-05-10 15:07:36 +0530

    Add files via upload

---
+++ q1.js
@@ -0,0 +1,128 @@
+alert("Hello");
+// Creating an array and passing the number, questions, options and answers
+
+let questions = [
+   {
+     numb: 1,
+     question: "Batman is also known as?",
+     answer: "Bruce Wayne",
+     options: [
+         "Bruce Lee",
+         "Bruce Banner",
+         "Bruce Wayne",
+         "Bruce John"
+     ]
+
+   },
+   {
+     numb: 2,
+     question: "Flash is also known as?",
+     answer: "Barry Allen",
+     options: [
+         "Barry Allen",
+         "Allen Barry",
+         "Allen Barri",
+         "Barri Allen"
+     ]
+
+   },
+   {
+     numb: 3,
+     question: "What is aquaman's weapon called as?",
+     answer: "Trident of Neptune",
+     options: [
+         "Trident of Amphitrite",
+         "Trident of Daxam",
+         "Trident of Neptune",
+         "Trident of Triton"
+     ]
+
+   },
+   {
+     numb: 4,
+     question: "Where did Wonder Woman come from?",
+     answer: "Themyscira",
+     options: [
+         "Themiscira",
+         "Myst world",
+         "Chomyst",
+         "Jemychiara"
+     ]
+
+   },
+   {
+     numb: 5,
+     question: "Which planet does Superman come from?",
+     answer: "Krypton",
+     options: [
+         "Daxam",
+         "Earth",
+         "Neptune",
+         "Krypton"
+     ]
+
+   },
+   {
+     numb: 6,
+     question: "Batman protects which city?",
+     answer: "Gotham City",
+     options: [
+         "Star City",
+         "Central City",
+         "Gotham City",
+         "Opal City"
+     ]
+
+   },
+   {
+     numb: 7,
+     question: "How did Spiderman get his powers?",
+     answer: "He was bitten by a spider",
+     options: [
+         "He was blinded with radioactive components",
+         "He was born with powers",
+         "He was affected by a chemical explosion",
+         "He was bitten by a spider"
+     ]
+
+   },
+   {
+     numb: 8,
+     question: "The 'Scarlett Speedster' is the name of which Superhero?",
+     answer: "The Flash",
+     options: [
+         "Speed Ball",
+         "The Flash",
+         "Star Dust",
+         "Hulk"
+     ]
+
+   },
+   {
+     numb: 9,
+     question: "Who was the first female superhero of the justice society of America?",
+     answer: "Wonder Woman",
+     options: [
+         "Elektra",
+         "Storm",
+         "Wonder Woman",
+         "Hawkgirl"
+     ]
+
+   },
+   {
+     numb: 10,
+     question: "What is Superman's real name in Krypto Planet series?",
+     answer: "Kal-el",
+     options: [
+         "Kal-el",
+         "Clark Kent",
+         "Jar-el",
+         "Herry Kent"
+     ]
+
+   }
+
+
+
+];
